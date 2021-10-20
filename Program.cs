5eed42d4c9480125264b21b6138ca1e368932f74
Joseph Bojovic <jbojovic@erpc.com>
2021-10-20 16:56:48 -0400

    Added a method to download images and create subfolders for the respected products. Added code to skip any blank rows.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using static System.Console;
-using ExcelObjects;
+
 
 
 namespace XlsxImageDownloader
@@ -15,11 +15,10 @@ namespace XlsxImageDownloader
         static void Main(string[] args)
         {
             string topLevelImagesFolder = @"C:\Users\jbojovic\Desktop\WebScraperTest\Images";
-            XlsxData fileDataParts = new XlsxData();
+            SheetDataSections fileDataParts = new SheetDataSections();
             fileDataParts.ProductNames = new List<string>();
             fileDataParts.SubProductCategories = new List<string>();
             fileDataParts.Urls = new List<string>();
-
 
 
             WriteLine("Please provide the following information.\n Enter the row number that the headers begin at.");
@@ -28,7 +27,7 @@ namespace XlsxImageDownloader
             WriteLine("Please enter the Column name where the product names are located.");
             fileDataParts.ProductNameColumn = ReadLine().ToLower().Replace(" ", "");
             WriteLine("Please enter the Column name where the sub-folder names are located.");
-            fileDataParts.SubProductFolderColumn = ReadLine().ToLower().Replace(" ", "");
+            fileDataParts.SubProductFolderColumn = ReadLine().ToLower().Replace(" ", "").Replace("-", "");
             WriteLine("Please enter the Column name where the URL's are located.");
             fileDataParts.UrlColumn = ReadLine().ToLower().Replace(" ", "");
 
@@ -50,18 +49,32 @@ namespace XlsxImageDownloader
                 // These 3 methods add all the product names, sub folder names and urls to their corresponding lists
                 LoopDownTheColumnAndAddDataToCorrespondingList(currentSheet, fileDataParts.ProductNameColumn,
                     fileDataParts.ProductNameColumn, fileDataParts.ProductNames, fileDataParts.StartingRowForLooping, lastRowInSheet);
-                WriteLine(fileDataParts.ProductNames.Count);
+                WriteLine($"Product Names: {fileDataParts.ProductNames.Count}");
 
 
                 LoopDownTheColumnAndAddDataToCorrespondingList(currentSheet, fileDataParts.SubProductFolderColumn,
                    fileDataParts.SubProductFolderColumn, fileDataParts.SubProductCategories, fileDataParts.StartingRowForLooping, lastRowInSheet);
-                WriteLine(fileDataParts.SubProductCategories.Count);
+                WriteLine($"Product SubFolders: {fileDataParts.SubProductCategories.Count}");
 
                 LoopDownTheColumnAndAddDataToCorrespondingList(currentSheet, fileDataParts.UrlColumn,
                    fileDataParts.UrlColumn, fileDataParts.Urls, fileDataParts.StartingRowForLooping, lastRowInSheet);
-                WriteLine(fileDataParts.Urls.Count);
+                WriteLine($"Product Urls: {fileDataParts.Urls.Count}");
+
+                ImageDownloader(currentSheet, fileDataParts.SubProductCategories, fileDataParts.ProductNames, fileDataParts.Urls);
+
+
+                // Clearing the lists to make sure all the rows line up with the correct information from each sheet. 
+                fileDataParts.ProductNames.Clear();
+                fileDataParts.SubProductCategories.Clear();
+                fileDataParts.Urls.Clear();
+
+                
+
 
             }
+
+
+
 
 
 
@@ -96,9 +109,10 @@ namespace XlsxImageDownloader
             {
                 // Getting the string value of the header column cells to then use in the if statements below. 
                 // We only want to get the data from certian columns that were specified in the beginning. 
-                HeaderTitle = currentSheet.Cell(loopStartRow, i).ValueAsString.ToLower().Replace(" ", "");
+                HeaderTitle = currentSheet.Cell(loopStartRow, i).ValueAsString.ToLower().Replace(" ", "").Replace("-", "");
 
                 // TODO Account for headers not being typed exactly (ie: parent category vs parent-category)
+                // Look into regexing this process
                 if (HeaderTitle.Equals(listObjectName))
                 {
 
@@ -106,7 +120,7 @@ namespace XlsxImageDownloader
                     // adding a +1 to pass the Header Row in the sheet.
                     for (int j = loopStartRow + 1; j <= lastRowInSheet; j++)
                     {
-                        if (currentSheet.Cell(j, i).ValueAsString != null)
+                        if (currentSheet.Cell(j, i).ValueAsString != null && currentSheet.Cell(j, i).ValueAsString != "")
                         {
                             listData.Add(currentSheet.Cell(j, i).ValueAsString);
                         }
@@ -118,8 +132,66 @@ namespace XlsxImageDownloader
         }
 
 
+        private static void ImageDownloader(Worksheet allSheetsInWorkbook, List<string> subCategories, List<string> productNames, List<string> downloadUrls)
+        {
 
-        public static async Task ExampleAsync(string errorLogData)
+
+            // Setting the top level folder that all the sub-folders will be created in when organizing the downloaded images.
+            string topLevelImagesFolder = @"C:\Users\jbojovic\Desktop\WebScraperTest\Images";
+
+
+            try
+            {
+                for (int i = 0; i <= 10; i++)
+                //for (int i = 0; i < prodNames.Count; i++)
+                {
+                    // Looping through the list of product categories to set the folder names for the nested loop. 
+                    string productTypeFolder = subCategories[i];
+
+                    for (int j = i; j <= 10;)
+                    //for (int j = i; j <= urls.Count;)
+                    {
+                        try
+                        {
+                            if (Directory.CreateDirectory(topLevelImagesFolder + "\\" + productTypeFolder).Exists)
+                            {
+                                string webAddress = downloadUrls[j];
+                                WebClient Client = new WebClient();
+                                Client.DownloadFile(webAddress, $@"{topLevelImagesFolder}\{productTypeFolder}\{productNames[i]}.jpg");
+                                //WriteLine($"Item: {prodNames[i]} | Link: {urls[j]}");
+
+                            }
+                            // This else statement will create the sub-folder if it does not exist then add the images to the folder with the product name. 
+                            else
+                            {
+                                Directory.CreateDirectory(topLevelImagesFolder + "\\" + productTypeFolder);
+                                string webAddress = downloadUrls[j];
+                                WebClient Client = new WebClient();
+                                Client.DownloadFile(webAddress, $@"{topLevelImagesFolder}\{productTypeFolder}\{productNames[i]}.jpg");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            WriteLine($"An exception has been caught: {ex.Message}. It is on item {productNames[i]} and the link is {downloadUrls[j]}");
+                            Task t = ErrorLog(DateTime.Now + "\r" + productNames[i] + "\r" + downloadUrls[j] + "\r");
+                        }
+                        break;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+                Task t = ErrorLog(DateTime.Now + " : " + ex.Message);
+                return;
+            }
+
+            WriteLine("Files have been created");
+        }
+
+
+        public static async Task ErrorLog(string errorLogData)
         {
             using StreamWriter file = new(@"C:\Users\jbojovic\Desktop\FailedUrls.txt", append: true);
             await file.WriteLineAsync(errorLogData);
---
+++ SheetDataSections.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XlsxImageDownloader
+{
+    class SheetDataSections
+    {
+        public List<string> MainProductCategories { get; set; }
+        public List<string> SubProductCategories { get; set; }
+        public List<string> ProductNames { get; set; }
+        public List<string> Urls { get; set; }
+        public string UrlColumn { get; set; }
+        public string ProductNameColumn { get; set; }
+        public string SubProductFolderColumn { get; set; }
+        public int StartingRowForLooping { get; set; }
+
+        public SheetDataSections()
+        {
+
+        }
+
+    }
+}
