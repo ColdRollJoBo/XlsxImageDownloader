62c26a1127af9bb8a64340a5614c8d2fbe6da1e0
Joseph Bojovic <jbojovic@erpc.com>
2021-10-28 17:02:47 -0400

    Rewritten Excel image scraper.

--- Program.cs
+++ Program.cs
@@ -13,129 +13,28 @@ namespace XlsxImageDownloader
 {
     class Program
     {
+        static bool triedAlready = false;
         static void Main(string[] args)
         {
             WriteLine("Program Running...");
-            // The Spreadsheet Class is the top most level of the document.
+            // The Spreadsheet Class is the whole excel file at the top most level.
             // The workbook is one step in and is mostly what is used to get actual data.
             Spreadsheet vendorDoc = new Spreadsheet();
-            vendorDoc.LoadFromFile(@"C:\Users\jbojovic\Desktop\WebScraperTest\destaco1pageonly.xlsx");
+            vendorDoc.LoadFromFile(@"C:\Users\jbojovic\Desktop\WebScraperTest\destaco3Column.xlsx");
 
             Workbook vendorProductCatalog = vendorDoc.Workbook;
             int numberOfSheetsInCatalog = vendorProductCatalog.Worksheets.Count;
-
-            // Setting this up so users can pick what columns they would like to use for data and using numbers
-            // instead of typing the full name.
-            // Dictionary<int, string> userMenuOptions = new Dictionary<int, string>();
 
             foreach (string sheet in CollectionOfAllSheetNames(vendorProductCatalog, numberOfSheetsInCatalog))
             {
 
                 CreateMainProductFolder(sheet);
+
                 LoopDownRowsInCurrentSheetAndDownloadImagesToCorrectFolders(vendorProductCatalog, sheet);
 
-
-
-                // short-circuting out of the foreach loop
-                break;
-
-
             }
-
-            WriteLine("Broke out of loop");
-
-
-
-
-
-
-            //string topLevelImagesFolder = @"C:\Users\jbojovic\Desktop\WebScraperTest\Images";
-            //SheetDataSections fileDataParts = new SheetDataSections();
-
-
-
-            //    fileDataParts.ProductNames = new List<string>();
-            //    fileDataParts.SubProductCategories = new List<string>();
-            //    fileDataParts.Urls = new List<string>();
-
-
-            //    WriteLine("Please provide the following information.\n Enter the row number that the headers begin at.");
-            //    //The starting row value has a minus one to account for the zero base indexing that excel uses.
-            //    fileDataParts.StartingRowForLooping = int.Parse(ReadLine()) - 1;
-            //    WriteLine("Please enter the Column name where the product names are located.");
-            //    fileDataParts.ProductNameColumn = ReadLine().ToLower().Replace(" ", "");
-            //    WriteLine("Please enter the Column name where the sub-folder names are located.");
-            //    fileDataParts.SubProductFolderColumn = ReadLine().ToLower().Replace(" ", "").Replace("-", "");
-            //    WriteLine("Please enter the Column name where the URL's are located.");
-            //    fileDataParts.UrlColumn = ReadLine().ToLower().Replace(" ", "");
-
-
-            //    Spreadsheet document = new Spreadsheet();
-            //    document.LoadFromFile(@"C:\Users\jbojovic\Desktop\WebScraperTest\destaco.xlsx");
-            //    int numberOfSheets = document.Workbook.Worksheets.Count;
-
-
-
-
-            //    foreach (string sheetName in CollectionOfAllSheetNames(document, numberOfSheets))
-            //    {
-
-            //        Worksheet currentSheet = document.Workbook.Worksheets.ByName(sheetName);
-
-            //        // Getting the last row in the sheet to use as the stopping point in the for loop.
-            //        int lastRowInSheet = currentSheet.Rows.LastFormatedRow;
-
-
-
-            //        // These 3 methods add all the product names, sub folder names and urls to their corresponding lists
-            //        LoopDownTheColumnAndAddDataToCorrespondingList(currentSheet, fileDataParts.ProductNameColumn,
-            //            fileDataParts.ProductNameColumn, fileDataParts.ProductNames, fileDataParts.StartingRowForLooping, lastRowInSheet);
-            //        WriteLine($"Product Names: {fileDataParts.ProductNames.Count}");
-
-
-            //        LoopDownTheColumnAndAddDataToCorrespondingList(currentSheet, fileDataParts.SubProductFolderColumn,
-            //           fileDataParts.SubProductFolderColumn, fileDataParts.SubProductCategories, fileDataParts.StartingRowForLooping, lastRowInSheet);
-            //        WriteLine($"Product SubFolders: {fileDataParts.SubProductCategories.Count}");
-
-            //        LoopDownTheColumnAndAddDataToCorrespondingList(currentSheet, fileDataParts.UrlColumn,
-            //           fileDataParts.UrlColumn, fileDataParts.Urls, fileDataParts.StartingRowForLooping, lastRowInSheet);
-            //        WriteLine($"Product Urls: {fileDataParts.Urls.Count}");
-
-
-            //        if (fileDataParts.ProductNames.Count == fileDataParts.Urls.Count && fileDataParts.ProductNames.Count == fileDataParts.SubProductCategories.Count)
-            //        {
-            //            ImageDownloader(currentSheet, fileDataParts.SubProductCategories, fileDataParts.ProductNames, fileDataParts.Urls);
-            //        }
-            //        else
-            //        {
-            //            WriteLine($"Sheet: {sheetName} does not have equal parts in all 3 lists");
-            //            Task t = ErrorLog($"\rSheet: {sheetName} does not have equal parts in all 3 lists\r");
-            //        }
-
-
-            //        // Clearing the lists to make sure all the rows line up with the correct information from each sheet. 
-            //        fileDataParts.ProductNames.Clear();
-            //        fileDataParts.SubProductCategories.Clear();
-            //        fileDataParts.Urls.Clear();
-
-
-
-
-            //    }
-
-
-
-
-
-
-
-
-            //    ReadLine();
-
-
-
-
-
+            // For single Sheet Downloads by sheet name
+            //LoopDownRowsInCurrentSheetAndDownloadImagesToCorrectFolders(vendorProductCatalog, "Conveyors");
         }
 
         public static List<string> CollectionOfAllSheetNames(Workbook catalog, int sheets)
@@ -156,6 +55,7 @@ namespace XlsxImageDownloader
         {
             // Setting the folder that all the other folders will be created in when organizing the downloaded images.
             string topLevelImagesFolder = @"C:\Users\jbojovic\Desktop\WebScraperTest\Images";
+            // Creating the new Sub-Category images will be organized into.
             Directory.CreateDirectory(topLevelImagesFolder + "\\" + folderName);
 
         }
@@ -164,9 +64,10 @@ namespace XlsxImageDownloader
         {
             Worksheet currentSheet = book.Worksheets.ByName(sheet);
             List<string> rowInfo = new List<string>();
+            string mainImageFolder = @"C:\Users\jbojovic\Desktop\WebScraperTest\Images";
 
             //  'i' is starting at 1 because there is header row describing the data below it.
-            for (int i = 1; i <= currentSheet.Rows.LastFormatedRow - 1; i++)
+            for (int i = 1; i <= currentSheet.Rows.LastFormatedRow; i++)
             {
                 for (int j = 0; j <= currentSheet.Columns.LastFormatedColumn; j++)
                 {
@@ -200,131 +101,82 @@ namespace XlsxImageDownloader
                 if (rowInfo.Count.Equals(3))
                 {
                     WriteLine($"{rowInfo[0]} : {rowInfo[1]} : {rowInfo[2]} ");
+                    DownloadImages(mainImageFolder, sheet, rowInfo);
+                }
+                else if (rowInfo.Count != 0)
+                {
+                    Task t = ErrorLog($"Row : {i} does not have all 3 parts needed to successfully download an item.");
                 }
                 rowInfo.Clear();
             }
 
 
+
         }
 
-        public static void CreatingMenuForUserToPickColumnsToUseForData(Workbook book, string sheet, Dictionary<int, string> menuOptions)
+
+        public static void DownloadImages(string topLevelImagesFolder, string sheet, List<string> itemParts)
         {
-            Worksheet currentSheet = book.Worksheets.ByName(sheet);
+            // Item parts consist of the name of the product, the sub-catagory and the url.
 
-            // Looping through the columns in the current sheet.
-            for (int i = 0; i <= currentSheet.Columns.LastFormatedColumn; i++)
-            {
-                if (currentSheet.Cell(4, i).ValueAsString.ToLower().Contains("product") ||
-                    currentSheet.Cell(4, i).ValueAsString.ToLower().Contains("url") ||
-                    currentSheet.Cell(4, i).ValueAsString.ToLower().Contains("category"))
+
+            try
+            {   // Checking to see if the subfolder already exists. If so then the product is added to the folder.
+                if (Directory.CreateDirectory(topLevelImagesFolder + "\\" + sheet + "\\" + itemParts[1]).Exists)
                 {
+                    string webAddress = itemParts[2];
+                    WebClient client = new WebClient();
+                    client.DownloadFile(webAddress, $@"{topLevelImagesFolder}\{sheet}\{itemParts[1]}\{itemParts[0]}.jpg");
 
-                    menuOptions.Add(i, currentSheet.Cell(4, i).ValueAsString);
+
+
+                }
+                // This else statement will create the sub-folder if it does not exist then add the images to the folder with the product name. 
+                else
+                {
+                    Directory.CreateDirectory(topLevelImagesFolder + "\\" + sheet + "\\" + itemParts[1]);
+                    string webAddress = itemParts[2];
+                    WebClient client = new WebClient();
+                    client.DownloadFile(webAddress, $@"{topLevelImagesFolder}\{sheet}\{itemParts[1]}\{itemParts[0]}.jpg");
+
                 }
             }
+            catch (Exception ex)
+            {
+                if (!triedAlready)
+                {
+                    triedAlready = true;
+                    Thread.Sleep(2000);
+                    RetryDownload(topLevelImagesFolder, sheet, itemParts);
+                }
+                else
+                {
+                    WriteLine($"An exception has been caught: {ex.Message}. It is on item {itemParts[0]} and the link is {itemParts[2]}.");
+                    Task t = ErrorLog(DateTime.Now + "\r" + sheet + "\r" + itemParts[1] + "\r" + itemParts[0] + "\r" + itemParts[2] + "\r");
+
+                }
+
+            }
+
 
         }
 
-        //public static void LoopDownTheColumnAndAddDataToCorrespondingList(Worksheet currentSheet, string HeaderTitle, string SubCategoryName, List<string> listData, int loopStartRow, int lastRowInSheet)
-        //{
-        //    // Looping through the columns in the current sheet.
-        //    for (int i = 0; i <= currentSheet.Columns.LastFormatedColumn; i++)
-        //    {
-        //        // Getting the string value of the header column cells to then use in the if statements below. 
-        //        // We only want to get the data from certian columns that were specified in the beginning. 
-        //        HeaderTitle = currentSheet.Cell(loopStartRow, i).ValueAsString.ToLower().Replace(" ", "").Replace("-", "");
+        public static void RetryDownload(string topLevelImagesFolder, string sheet, List<string> itemParts)
+        {
+            //Retrying to download the image again.
+            DownloadImages(topLevelImagesFolder, sheet, itemParts);
 
-        //        // TODO: Account for headers not being typed exactly (ie: parent category vs parent-category)
-        //        // Look into regexing this process
-        //        if (HeaderTitle.Equals(SubCategoryName))
-        //        {
+            Task t = ErrorLog(DateTime.Now + " Retried download, check for success" +
+                "\r" + sheet + "\r" + itemParts[1] + "\r" + itemParts[0] + "\r" + itemParts[2] + "\r");
 
-        //            // Looping down the columns to add the data to their associated lists.
-        //            // adding a +1 to pass the Header Row in the sheet.
-        //            for (int j = loopStartRow + 1; j <= lastRowInSheet; j++)
-        //            {
-        //                if (currentSheet.Cell(j, i).ValueAsString != null && currentSheet.Cell(j, i).ValueAsString != "")
-        //                {
-        //                    listData.Add(currentSheet.Cell(j, i).ValueAsString);
-        //                }
+            triedAlready = false;
 
-        //            }
-        //        }
-        //    }
-
-        //}
-
-
-        //private static void ImageDownloader(Worksheet sheet, List<string> subCategories, List<string> productNames, List<string> downloadUrls)
-        //{
-
-
-        //    // Setting the top level folder that all the sub-folders will be created in when organizing the downloaded images.
-        //    string topLevelImagesFolder = @"C:\Users\jbojovic\Desktop\WebScraperTest\Images";
-
-
-
-        //    try
-        //    {
-        //        // Limiting the amount of products to loop through. uncommment the for loop underneath for all products.
-        //        //for (int i = 0; i <= 10; i++)
-        //        for (int i = 0; i < productNames.Count; i++)
-        //        {
-        //            // Looping through the list of product categories to set the folder names for the nested loop. 
-        //            string productTypeFolder = subCategories[i];
-
-        //            // Limiting the amount of urls to loop through. uncommment the for loop underneath for all urls.
-        //            //for (int j = i; j <= 10;)
-        //            for (int j = i; j <= downloadUrls.Count;)
-        //            {
-        //                try
-        //                {   // Checking to see if the subfolder already exists. If so then the product is added to the folder.
-        //                    if (Directory.CreateDirectory(topLevelImagesFolder + "\\" + sheet.Name + "\\" + productTypeFolder).Exists)
-        //                    {
-        //                        string webAddress = downloadUrls[j];
-        //                        WebClient Client = new WebClient();
-        //                        Client.DownloadFile(webAddress, $@"{topLevelImagesFolder}\{sheet.Name}\{productTypeFolder}\{productNames[i]}.jpg");
-        //                        //WriteLine($"Item: {prodNames[i]} | Link: {urls[j]}");
-
-        //                    }
-        //                    // This else statement will create the sub-folder if it does not exist then add the images to the folder with the product name. 
-        //                    else
-        //                    {
-        //                        Directory.CreateDirectory(topLevelImagesFolder + "\\" + sheet.Name + "\\" + productTypeFolder);
-        //                        string webAddress = downloadUrls[j];
-        //                        WebClient Client = new WebClient();
-        //                        Client.DownloadFile(webAddress, $@"{topLevelImagesFolder}\{sheet.Name}\{productTypeFolder}\{productNames[i]}.jpg");
-        //                    }
-        //                }
-        //                catch (Exception ex)
-        //                {
-        //                    WriteLine($"An exception has been caught: {ex.Message}. It is on item {productNames[i]} and the link is {downloadUrls[j]}");
-        //                    Task t = ErrorLog(DateTime.Now + "\r" + sheet.Name + "\r" + productTypeFolder + "\r" + productNames[i] + "\r" + downloadUrls[j] + "\r");
-
-        //                    string webAddress = downloadUrls[j];
-
-
-        //                }
-        //                //Thread.Sleep(500);
-        //                break;
-        //            }
-        //        }
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        WriteLine(ex.Message);
-        //        Task t = ErrorLog(DateTime.Now + " : " + ex.Message);
-        //        return;
-        //    }
-
-        //    WriteLine("Files have been created");
-        //}
+        }
 
 
         public static async Task ErrorLog(string errorLogData)
         {
-            using StreamWriter file = new(@"C:\Users\jbojovic\Desktop\FailedUrls.txt", append: true);
+            using StreamWriter file = new(@"C:\Users\jbojovic\Desktop\WebScraperTest\FailedUrls.txt", append: true);
             await file.WriteLineAsync(errorLogData);
         }
     }
